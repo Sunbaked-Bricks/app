5d101cf23e8b1ea868a60bd7714c09b329933abb
Parker <pcollie4@vols.utk.edu>
2023-02-28 13:41:11 -0500

    reading fikles

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,42 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/counter.txt');
+}
+
+Future<File> writeMessage(String message) async {
+  final file = await _localFile;
+
+  // Write the file
+  return file.writeAsString(message);
+}
+
+Future<String> readMessage() async {
+  try {
+    final file = await _localFile;
+
+    // Read the file
+    final contents = await file.readAsString();
+
+    return contents;
+  } catch (e) {
+    // If encountering an error, return 0
+    return 'error reading file';
+  }
+}
+
 Future<int> createTelegram(String title) async {
   final response = await http.post(
     Uri.parse(
